a77edf80893d61f8b5510d9a79e88e781f596793
Kalyanam Pavan Kumar <pavan4404kpk@gmail.com>
2025-05-25 14:14:02 +0530

    Admin page components

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -76,4 +76,15 @@ const adminLogin = async (req, res) => {
     }
 }
 
-export { addDoctor, adminLogin };
+//API to get all doctors for admin
+const allDoctors = async (req, res) => {
+    try {
+        const doctors = await doctorModel.find({}).select("-password");
+        res.json({success: true, doctors});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+export { addDoctor, adminLogin, allDoctors };
